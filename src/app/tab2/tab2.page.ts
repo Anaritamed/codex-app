8492f28954c127057eee4c761b01a3570b76d1fa
Anaritamed <100716949+Anaritamed@users.noreply.github.com>
2023-05-25 18:41:35 -0300

    2 arquivos modificados

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -5,28 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
 
-  TodoList = [{
-    itemName: 'Codigo',
-    itemDueDate: '10-13-23',
-    itemPriority: 'high',
-    itemCategory: 'Trabalho'
-  },
-  {
-    itemName: 'Design',
-    itemDueDate: '28-10-23',
-    itemPriority: 'low',
-    itemCategory: 'Trabalho'
-  },
-  {
-    itemName: 'Casa',
-    itemDueDate: '30-10-23',
-    itemPriority: 'middle',
-    itemCategory: 'Pessoal'
-  },
-  ]
-
-  today : number = Date.now();
+export class Tab2Page{
   constructor() {}
 }
