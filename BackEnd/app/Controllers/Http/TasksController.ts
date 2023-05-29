2cd6f69378ba3a381632a5a1d738ce83f59e7ef6
Ananda Vilar Vidal <ananda.vilar.vidal@ccc.ufcg.edu.br>
2023-05-28 21:45:15 -0300

    final commit

--- BackEnd/app/Controllers/Http/TasksController.ts
+++ BackEnd/app/Controllers/Http/TasksController.ts
@@ -1,12 +1,27 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Task from 'App/Models/Task'
 
+/**
+ * Responsável por exibir, armazenar, atualizar e remover tarefas 
+ */
 export default class TasksController {
+  /**
+   * Exibe todas as tarefas
+   * 
+   * @param param0 
+   * @returns todas as tarefas
+   */
   public async index({}: HttpContextContract) {
     const task = await Task.all()
     return task
   }
 
+  /**
+   * Armazena a tarefa no banco de dados
+   * 
+   * @param param0 tarefa
+   * @returns a tarefa salva
+   */
   public async store({ request }: HttpContextContract) {
     const body = request.only(['tarefa'])
 
@@ -16,6 +31,12 @@ export default class TasksController {
     return task
   }
 
+  /**
+   * Atualiza a tarefa no banco de dados
+   * 
+   * @param param0 id, tarefa
+   * @returns a tarefa já atualizada
+   */
   public async update({ request }: HttpContextContract) {
     const taskId = request.param('id')
     const body = request.only(['tarefa'])
@@ -24,6 +45,12 @@ export default class TasksController {
     return task
   }
 
+  /**
+   * Remove uma tarefa específica do banco de dados
+   * 
+   * @param param0 id
+   * @returns true
+   */
   public async destroy({ request }: HttpContextContract) {
     const taskId = request.param('id')
     const task = await Task.findOrFail(taskId)
--- BackEnd/app/Controllers/Http/UsersController.ts
+++ BackEnd/app/Controllers/Http/UsersController.ts
@@ -1,16 +1,53 @@
+import { v4 as uuidv4 } from 'uuid'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User';
+import Application from '@ioc:Adonis/Core/Application'
 
+/**
+ * Responsável por exibir, armazenar, atualizar e remover usuários 
+ */
 export default class UsersController {
+  /**
+   * Valida os "critérios" da imagem que será adicionada
+   */
+  private validationOptions = {
+    types: ['image'],
+    size: '2mb',
+  }
+
+  /**
+   * Exibe todos os usuários
+   * 
+   * @param param0 
+   * @returns todos os usuários
+   */
   public async index({}: HttpContextContract) {
     const user = await User.all()
     return user
   }
 
+  /**
+   * Armazena os dados do usuário no banco de dados
+   * 
+   * @param param0 image, nome, genero, idade, email, password
+   * @returns o usuário salvo
+   */
   public async store({ request }: HttpContextContract) {
-    const body = request.only(['nome', 'genero', 'idade', 'email', 'password'])
+    const body = request.only(['image', 'nome', 'genero', 'idade', 'email', 'password'])
+    const image = request.file('image', this.validationOptions)
+
+    if (image) {
+      const imageName = `${uuidv4()}.${image!.extname}`
+
+      await image.move(Application.tmpPath('uploads'), {
+        name: imageName
+      })
+
+      body.image = imageName
+    }
 
     const user = await User.create({
+      image: body.image,
       nome: body.nome,
       genero: body.genero,
       idade: body.idade,
@@ -20,20 +57,52 @@ export default class UsersController {
     return user
   }
 
+  /**
+   * Exibe os dados de um usuário específico
+   * 
+   * @param param0 id
+   * @returns o usuário solicitado
+   */
   public async show({ request }: HttpContextContract) {
     const userId = request.param('id')
     const user = await User.findOrFail(userId)
     return user
   }
 
+  /**
+   * Atualiza os dados de um usuário no banco de dados
+   * 
+   * @param param0 id, image, nome, genero, idade, email, password
+   * @returns o usuário já atualizado
+   */
   public async update({ request }: HttpContextContract) {
     const userId = request.param('id')
-    const body = request.only(['nome', 'genero', 'idade', 'email', 'password'])
+    const body = request.only(['image', 'nome', 'genero', 'idade', 'email', 'password'])
     const user = await User.findOrFail(userId)
+
+    //Verifica se a imagem já está armazenada ou se não existe no banco de dados
+    if (user.image != body.image || !user.image) {
+      const image = request.file('image', this.validationOptions)
+      if (image) {
+        const imageName = `${uuidv4()}.${image!.extname}`
+  
+        await image.move(Application.tmpPath('uploads'), {
+          name: imageName
+        })
+  
+        user.image = imageName
+      }
+    }
     await user.merge(body).save()
     return user
   }
 
+  /**
+   * Remove um usuário específico do banco de dados
+   * 
+   * @param param0 id
+   * @returns true
+   */
   public async destroy({ request }: HttpContextContract) {
     const userId = request.param('id')
     const user = await User.findOrFail(userId)
